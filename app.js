746027e1b565e85d2f211d41d66a3deeb4b8e327
Victoria <94654318+victoriacferrari@users.noreply.github.com>
2025-09-22 14:58:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 // Array para armazenar os nomes dos amigos
 let amigos = [];
 
@@ -73,3 +72,4 @@ function reiniciar() {
     lista.innerHTML = "";
     lista.classList.remove("esconder");
 }
+
